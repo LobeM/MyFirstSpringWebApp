a5185e67bb97f7d7a2e4236803858112cac7dab6
Lobe <lobe350@gmail.com>
2025-03-27 11:00:09 +0200

    Added navigation bar and implemented JSP fragments

--- src/main/java/com/lobemusonda/myfirstspringwebapp/todo/TodoController.java
+++ src/main/java/com/lobemusonda/myfirstspringwebapp/todo/TodoController.java
@@ -4,7 +4,10 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.time.LocalDate;
 import java.util.List;
